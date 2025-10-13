d6bfbe629b6d1aea1b95e4cdac32e6da35959a7f
Peyrona <peyrona@users.noreply.github.com>
2025-10-13 04:40:16 +0200

    Clean up: Remove tracked files specified in .gitignore

--- lang/build/classes/com/peyrona/mingle/lang/xpreval/functions/SunriseSunSet.rs
+++
@@ -1,3 +0,0 @@
-com.peyrona.mingle.lang.xpreval.functions.SunriseSunset$SolarEquationVariables
-com.peyrona.mingle.lang.xpreval.functions.SunriseSunset$DayPeriod
-com.peyrona.mingle.lang.xpreval.functions.SunriseSunset
